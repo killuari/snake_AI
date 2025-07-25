22fb1de899497b3ea9a5338f434d33b84a74c787
killuari <killuari@protonmail.com>
2025-07-25 21:31:45 +0200

    Add first Snake part

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+from enum import Enum
 
 # pygame setup
 pygame.init()
@@ -7,7 +8,37 @@ clock = pygame.time.Clock()
 running = True
 dt = 0
 
+class Direction(Enum):
+    LEFT = 0
+    RIGHT = 1
+    UP = 2
+    DOWN = 3
+
+class Snake:
+    pass
+
+class SnakePart:
+    def __init__(self, screen, pos):
+        self.screen = screen
+        self.pos = pos
+
+    def draw(self):
+        pygame.draw.rect(self.screen, pygame.Color(0, 200, 0), pygame.Rect(self.pos.x, self.pos.y, 20, 20))
+
+    def move(self, dir: Direction, dt):
+        if dir == Direction.LEFT:
+            self.pos.x -= 300 * dt
+        elif dir == Direction.RIGHT:
+            self.pos.x += 300 * dt
+        elif dir == Direction.UP:
+            self.pos.y -= 300 * dt
+        elif dir == Direction.DOWN:
+            self.pos.y += 300 * dt
+
 player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
+dir = Direction.RIGHT
+
+p1 = SnakePart(screen, player_pos)
 
 while running:
     for event in pygame.event.get():
@@ -18,13 +49,17 @@ while running:
 
     keys = pygame.key.get_pressed()
     if keys[pygame.K_w]:
-        player_pos.y -= 300 * dt
+        dir = Direction.UP
     if keys[pygame.K_s]:
-        player_pos.y += 300 * dt
+        dir = Direction.DOWN
     if keys[pygame.K_a]:
-        player_pos.x -= 300 * dt
+        dir = Direction.LEFT
     if keys[pygame.K_d]:
-        player_pos.x += 300 * dt
+        dir = Direction.RIGHT
+
+    p1.move(dir, dt)
+    p1.draw()
+
 
     pygame.display.flip()
 
